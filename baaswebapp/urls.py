536611a0da3e4a391ecc165023f6625bde172fea
jsantiagoProbelte <jsantiago@probelte.com>
2022-11-14 23:58:53 +0100

    adding excplictly STATIC_ROOT

--- baaswebapp/urls.py
+++ baaswebapp/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from baaswebapp import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 admin.autodiscover()
 
@@ -30,4 +31,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+urlpatterns += staticfiles_urlpatterns() +\
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
