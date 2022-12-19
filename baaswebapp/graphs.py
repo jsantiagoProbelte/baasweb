b8d97901c9f4260e478b9d91ca94a0317f6855f0
jsantiagoProbelte <jsantiago@probelte.com>
2022-12-20 00:28:54 +0100

    scatter plot with colors and symbols

--- baaswebapp/graphs.py
+++ baaswebapp/graphs.py
@@ -2,6 +2,42 @@
 from plotly.offline import plot
 import plotly.graph_objs as go
 
+COLOR_main_color = '#a500a5'
+COLOR_red = '#ff6e73'
+COLOR_yellow = '#ffd26e'
+COLOR_green = '#92eeb9'
+COLOR_blue = '#00bdeb'
+COLOR_grey = '#F0EEEB'
+COLOR_black = '#333333'
+COLOR_bio_morado = '#aa4ae4'
+COLOR_morado = '#a500a5'
+COLOR_violeta = '#cfc9ff'
+COLOR_bs_blue = '#325d88'
+COLOR_bs_indigo = '#6610f2'
+COLOR_bs_purple = '#6f42c1'
+COLOR_bs_pink = '#e83e8c'
+COLOR_bs_red = '#d9534f'
+COLOR_bs_orange = '#f47c3c'
+COLOR_bs_yellow = '#ffc107'
+COLOR_bs_green = '#93c54b'
+COLOR_bs_teal = '#20c997'
+COLOR_bs_cyan = '#2997FF'
+COLOR_bs_white = '#fff'
+COLOR_bs_gray = '#8e8c84'
+COLOR_bs_gray_dark = '#3e3f3a'
+COLOR_bs_primary = '#325d88'
+COLOR_bs_secondary = '#8e8c84'
+COLOR_bs_success = '#93c54b'
+COLOR_bs_info = '#29abe0'
+COLOR_bs_warning = '#f47c3c'
+COLOR_bs_danger = '#d9534f'
+COLOR_bs_light = '#f8f5f0'
+COLOR_bs_dark = '#3e3f3a'
+COLOR_bs_text_color = '#f4f4f4'
+COLOR_bs_text_color_table = '#fff'
+COLOR_bg_color = '#282828'
+COLOR_bg_color_cards = '#333333'
+
 
 class Graph:
     _graphData = []
@@ -11,6 +47,18 @@ class Graph:
     L_THESIS = 'thesis'
     L_REPLICA = 'replica'
 
+    # SymbolValidator().values
+    SYMBOL_LIST = ['cicle', 'square', 'star', 'diamond', 'cross',
+                   'x', 'triangle-up', 'triangle-down',
+                   'triangle-left', 'triangle-right']
+
+    COLOR_LIST = [COLOR_bg_color, COLOR_morado, COLOR_bio_morado,
+                  COLOR_violeta,
+                  COLOR_red, COLOR_yellow, COLOR_green, COLOR_blue,
+                  COLOR_bs_primary, COLOR_bs_success, COLOR_bs_info,
+                  COLOR_bs_warning, COLOR_bs_danger,
+                  COLOR_bs_green, COLOR_bs_secondary]
+
     def __init__(self, level,
                  trialAssessments, dataPoints):
         self._graphData = self.buildData(level,
@@ -19,6 +67,7 @@ class Graph:
     def preparePlots(self, typeFigure='scatter', orientation='v'):
         graphPlots = [self.figure(
             item['x'], item['y'], title=item['title'],
+            colors=item['colors'], symbols=item['symbols'],
             typeFigure=typeFigure, orientation=orientation,
             xaxis_title=item['x_axis'], yaxis_title=item['y_axis'])
                 for item in self._graphData]
@@ -33,6 +82,7 @@ class Graph:
     def figure(self, theX, theY,
                typeFigure=SCATTER, orientation='v',
                name=None, title=None,
+               colors=[COLOR_morado], symbols=['cicle'],
                xaxis_title=None, yaxis_title=None):
         data = None
         if typeFigure == Graph.BAR:
@@ -40,6 +90,7 @@ class Graph:
                           name=name, x=theY, y=theX)
         elif typeFigure == Graph.SCATTER:
             data = go.Scatter(name=name, x=theX, y=theY,
+                              marker={'color': colors, 'symbol': symbols},
                               mode='markers', marker_size=15)
         fig = go.Figure(data)
         # Update layout for graph object Figure
@@ -87,16 +138,30 @@ class Graph:
                 'y_axis': unit.unit.name}
             theX = []
             theY = []
+            theSymbols = []
+            theColors = []
             for dataPoint in dataPoints:
                 if unit.id == dataPoint.unit.id:
                     xValue = None
+                    symbol = 'star'
                     if level == Graph.L_THESIS:
                         xValue = dataPoint.reference.name
+                        color = Graph.COLOR_LIST[
+                            dataPoint.reference.number]
                     elif level == Graph.L_REPLICA:
+                        symbol = Graph.SYMBOL_LIST.get(
+                            dataPoint.reference.number, 'star')
+                        color = Graph.COLOR_LIST.get(
+                            dataPoint.reference.thesis.number,
+                            COLOR_main_color)
                         xValue = dataPoint.reference.thesis.name
                     theX.append(xValue)
                     theY.append(dataPoint.value)
+                    theColors.append(color)
+                    theSymbols.append(symbol)
             thisGraph['x'] = theX
             thisGraph['y'] = theY
+            thisGraph['symbols'] = theSymbols
+            thisGraph['colors'] = theColors
             graphs.append(thisGraph)
         return graphs
