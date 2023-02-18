11e05efa79ffd9a35f763b1fb7e81a125949a74d
jsantiagoProbelte <jsantiago@probelte.com>
2023-02-18 15:28:00 +0100

    add productthesis to admin

--- trialapp/admin.py
+++ trialapp/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 # Register your models here.
 from trialapp.models import FieldTrial, Crop, TrialType, Project, Objective,\
-    RateUnit, Plague, TrialStatus, AssessmentUnit, AssessmentType
+    RateUnit, Plague, TrialStatus, AssessmentUnit, AssessmentType, ProductThesis
 
 
 class FieldTrialAdmin(admin.ModelAdmin):
@@ -19,3 +19,4 @@ admin.site.register(RateUnit)
 admin.site.register(Plague)
 admin.site.register(AssessmentUnit)
 admin.site.register(AssessmentType)
+admin.site.register(ProductThesis)
