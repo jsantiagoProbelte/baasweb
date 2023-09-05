3e03c6f8ed2afe50bf33e23dd5e408e6b89ad5d7
Jose Santiago <jsantiago@probelte.com>
2023-09-05 20:20:22 +0200

    setting key assessment

--- trialapp/tests/tests_trial.py
+++ trialapp/tests/tests_trial.py
@@ -129,11 +129,11 @@ class TrialViewsTest(TestCase):
         self.assertEqual(trialH.whatIsKeyThesis(), self._keyThesis.id)
         self._trial = FieldTrial.objects.get(id=self._trial.id)
         self.assertEqual(self._trial.key_thesis, self._keyThesis.id)
-        self.assertEqual(self._trial.untreated_thesis, self._controlThesis.id)
+        self.assertEqual(self._trial.control_thesis, self._controlThesis.id)
 
         # validate that if there is data, even if it is wrong it  not check it
         self._trial.key_thesis = 66
-        self._trial.untreated_thesis = 66
+        self._trial.control_thesis = 66
         self._trial.save()
         trialH = TrialContent(self._trial.id, TrialContent.KEY_ASSESS)
         self.assertEqual(trialH.whatIsControlThesis(), 66)
@@ -143,20 +143,23 @@ class TrialViewsTest(TestCase):
                          self._controlThesis.id)
         self.assertEqual(trialH.whatIsKeyThesis(force=True),
                          self._keyThesis.id)
-
-        self.assertEqual(trialH.whatIsBestEfficacy(1), None)
-        self.assertEqual(trialH.whatIsBestEfficacy(1, force=True), 1)
+        self._trial = FieldTrial.objects.get(id=self._trial.id)
+        self.assertEqual(trialH.whatIsBestEfficacy(1, 1), None)
+        checkTrial = FieldTrial.objects.get(id=self._trial.id)
+        self.assertTrue(checkTrial.key_assessment is None)
+        self.assertTrue(checkTrial.best_efficacy is None)
+        self.assertEqual(trialH.whatIsBestEfficacy(1, 1, force=True), 1)
+        checkTrial = FieldTrial.objects.get(id=self._trial.id)
+        self.assertEqual(checkTrial.key_assessment, 1)
+        self.assertTrue(checkTrial.best_efficacy, 1)
 
         keyRate, keyPart = trialH.whatIsKeyRates()
         self.assertEqual(keyRate.id, self._firstRateTypeUnitId)
         # ... because all of the them are equal
 
         # Now let's calculate best efficacy
-        dataPoints = trialH.getKeyAssData(
-            keyRate, keyPart, self._keyThesis.id, self._controlThesis.id)
-        assmts = trialH.getKeyAssmts(keyRate, keyPart)
-        bestEfficacy, line = trialH.calculateBestEfficacy(
-            dataPoints, assmts, self._keyThesis.id, self._controlThesis.id)
+        dataPoints = trialH.getKeyEfficacyComponents()
+        bestEfficacy, line, kAssId = trialH.computeBestEfficacy(dataPoints)
         firstExpectedEff = trialH.calculateEfficacy(
             TrialViewsTest.CONTROL_VALUE, TrialViewsTest.FIRST_MIN_VALUE)
         self.assertEqual(bestEfficacy, firstExpectedEff)
@@ -169,6 +172,7 @@ class TrialViewsTest(TestCase):
         self.assertEqual(content['best_keytesis_value'], line['y'][1])
 
         # let's add one on other RateTypeUnit
+        self._trial = FieldTrial.objects.get(id=self._trial.id)
         keyRateTypeUnitId = self._firstRateTypeUnitId + 1
         newAss = self.addAssessment(keyRateTypeUnitId, datetime(2022, 12, 1))
         for replica in Replica.objects.all():
@@ -183,17 +187,27 @@ class TrialViewsTest(TestCase):
         self.assertEqual(keyRate.id, keyRateTypeUnitId)
 
         # if we calculate the efficacy is going to change
+        # after adding the assessment
+        self._trial = FieldTrial.objects.get(id=self._trial.id)
+        self._trial.key_assessment = None
+        self._trial.save()
         tContent = TrialContent(self._trial.id, TrialContent.KEY_ASSESS)
         content = tContent.fetchKeyAssessData()
         secondEfficacyExpected = tContent.calculateEfficacy(
             TrialViewsTest.CONTROL_VALUE, TrialViewsTest.LAST_MIN_VALUE)
         self.assertEqual(secondEfficacyExpected,
                          content['bestEfficacy'])
+        checkTrial = FieldTrial.objects.get(id=self._trial.id)
+        # it is an id, different from 1
+        self.assertTrue(checkTrial.key_assessment > 1)
 
         # if we change the type of product to not be a control,
         # calculation of efficacy will change
         self._botribel.type_product = PType.FERTILIZER
         self._botribel.save()
+        self._trial = FieldTrial.objects.get(id=self._trial.id)
+        self._trial.key_assessment = None
+        self._trial.save()
         tContent2 = TrialContent(self._trial.id, TrialContent.KEY_ASSESS)
         thirdEfficacyExpected = tContent2.calculateEfficacy(
             TrialViewsTest.CONTROL_VALUE, TrialViewsTest.FIRST_MIN_VALUE)
@@ -204,6 +218,26 @@ class TrialViewsTest(TestCase):
         # input numbers are the same, the output is different becasue the
         # type of product is different
         self.assertNotEqual(secondEfficacyExpected, thirdEfficacyExpected)
+
+    def test_computeBestEfficacy(self):
+        self.createObjects()
+        trialH = TrialContent(self._trial.id, TrialContent.KEY_ASSESS)
+        dataPoints = trialH.getKeyEfficacyComponents()
+        efficacy, line, fAssmId = trialH.calculateBestEfficacy(dataPoints)
+        self.assertTrue(efficacy is not None)
+        self.assertTrue(fAssmId is not None)
+
+        # Lets change to another assessment not optimal
+        self._trial.key_assessment = fAssmId + 1
+        self._trial.save()
+        trialH2 = TrialContent(self._trial.id, TrialContent.KEY_ASSESS)
+        dataPoints = trialH2.getKeyEfficacyComponents()
+        efficacy2, line, fAssmId2 = trialH2.calculateBestEfficacy(dataPoints)
+        self.assertTrue(fAssmId2 != fAssmId)
+
+        efficacy3, line, fAssmId3 = trialH2.calculateBestEfficacy(dataPoints,
+                                                                  force=True)
+        self.assertTrue(fAssmId3 == fAssmId)
 
     def test_fetches(self):
         # Fetch content before data is created.. to go through empty statments
