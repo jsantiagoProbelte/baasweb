12416f3fe8e835ac765e5cce653f4068c7783569
PROBELTE\angelbea <angelbea@dhop.tech>
2023-11-15 11:45:04 +0100

    fix: latitude and longitude null cause exception

--- trialapp/fieldtrial_views.py
+++ trialapp/fieldtrial_views.py
@@ -231,8 +231,8 @@ class FieldTrialCreateView(LoginRequiredMixin, CreateView):
         if form.is_valid():
             fieldTrial = form.instance
             fieldTrial.trial_meta = FieldTrial.TrialMeta.FIELD_TRIAL
-            fieldTrial.latitude = fieldTrial.latitude.replace(",", ".")
-            fieldTrial.longitude = fieldTrial.longitude.replace(",", ".")
+            fieldTrial.latitude = fieldTrial.latitude.replace(",", ".") if fieldTrial.latitude else None
+            fieldTrial.longitude = fieldTrial.longitude.replace(",", ".") if fieldTrial.longitude else None
             fieldTrial.save()
             EventLog.track(
                 EventBaas.NEW_TRIAL,
@@ -256,8 +256,8 @@ class FieldTrialUpdateView(LoginRequiredMixin, UpdateView):
         super().form_valid(form)
         TrialFile().createTrialFolder(self.get_object().code)
         fieldTrial = form.instance
-        fieldTrial.latitude = fieldTrial.latitude.replace(",", ".")
-        fieldTrial.longitude = fieldTrial.longitude.replace(",", ".")
+        fieldTrial.latitude = fieldTrial.latitude.replace(",", ".") if fieldTrial.latitude else None
+        fieldTrial.longitude = fieldTrial.longitude.replace(",", ".") if fieldTrial.longitude else None
         fieldTrial.save()
         EventLog.track(EventBaas.UPDATE_TRIAL,
                        self.request.user.id,
